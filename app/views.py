2e947baa6601d22ea2cd34a57afc8859f1cefd61
zoooesfG <zoe.ferguson@globacore.com>
2022-10-13 12:20:42 -0400

    form trial

---
+++ app/views.py
@@ -0,0 +1,16 @@
+
+from django.shortcuts import render
+from .forms import CustomerForm
+
+
+def index(request):
+
+	form = CustomerForm()
+
+	if request.method == 'POST':
+		form = CustomerForm(request.POST)
+		if form.is_valid():
+			form.save()
+			
+	context = {'form':form}
+	return render(request, 'app/index.html', context)
